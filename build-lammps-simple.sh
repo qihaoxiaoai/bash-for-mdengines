ffe94c7f7e84574e9f2f002c16d1d597c7e5a510
qihaoxiaoai <qihaoxiaoai@gmail.com>
2022-08-15 00:50:34 +0800

    lammps_install_bash

---
+++ build-lammps-simple.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+set -e
+module purge
+module load gcc/5.4.0
+module load openmpi/3.1.4-gnu-5.4.0
+export PATH=/home/xhshi/hjx/software-self/lammps-simple/env/fftw338s/bin:$PATH
+export C_INCLUDE_PATH=/home/xhshi/hjx/software-self/lammps-simple/env/fftw338s/include:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=/home/xhshi/hjx/software-self/lammps-simple/env/fftw338s/include:$CPLUS_INCLUDE_PATH
+source /home/xhshi/hjx/software/cmake-3.19.8-Linux-x86_64/env.sh
+source /home/xhshi/hjx/software-self/lammps-simple/old/source-plumed.sh
+INSTALL_DIR=/home/xhshi/hjx/software-self/lammps-simple/old/lmp_plu
+BuildMPI=yes
+LAMMPS_VERSION=stable_29Sep2021_update3
+Make_CPUs=20
+PLUMED_LinkMode=static
+#static;shared;runtime
+
+
+if [[  -z ${PLUMED_KERNEL} ]]; then
+  echo "The PLUMED environmental variables seen to be missing"
+  echo "Did you run source source-plumed.sh?"
+  exit 0
+fi
+
+if [[  -z ${PLUMED_INSTALL_DIR} ]]; then
+  echo "The PLUMED environmental variables seen to be missing"
+  echo "Did you run source source-plumed.sh?"
+  exit 0
+fi
+
+if  [[ ! -x "$(command -v plumed)" ]]; then
+  echo "The plumed command seems to be missing from the path" 
+  echo "Did you run source source-plumed.sh?"
+  exit 0
+fi
+
+if  ! plumed config -q has mpi  && [[ ${BuildMPI} == "yes" ]]; then 
+  echo "You are trying to compile LAMMPS with MPI while PLUMED has been compiled without MPI"
+  echo "Set BuildMPI to no in the script" 
+  exit 0
+fi
+
+
+# Download and build LAMMPS 
+if [[ -e ${LAMMPS_VERSION}.zip ]]; then
+  echo "the ${LAMMPS_VERSION}.zip that we want to download already exists, please delete it to be sure that the file is properly downloaded"
+  exit 0
+fi
+if [[ -e lammps-${LAMMPS_VERSION} ]]; then
+  echo "the lammps-${LAMMPS_VERSION} folder from unzipping the ${LAMMPS_VERSION}.zip already exists, please delete it to be sure that the build is clean"
+  exit 0
+fi
+
+wget --no-cookie --no-check-certificate -e robots=off https://github.com/lammps/lammps/archive/refs/tags/${LAMMPS_VERSION}.zip
+unzip ${LAMMPS_VERSION}.zip 
+rm -f ${LAMMPS_VERSION}.zip 
+cd lammps-${LAMMPS_VERSION}
+
+
+if [[ ${BuildMPI} == "no" ]]; then 
+  # Since this version is not compiled with MPI, it should not pass a communicator to PLUMED
+  cat src/USER-PLUMED/fix_plumed.cpp | grep -v setMPIComm > src/USER-PLUMED/fix_plumed.cpp.fix
+  mv src/USER-PLUMED/fix_plumed.cpp.fix src/USER-PLUMED/fix_plumed.cpp
+fi
+
+opt=""
+
+if [[ $(uname) == Darwin ]]; then
+  # Fix the name of the PLUMED kernel on OSX
+  # cmake:
+  cat cmake/CMakeLists.txt | sed "s/libplumedKernel.so/libplumedKernel.dylib/" > cmake/CMakeLists.txt.fix
+  mv cmake/CMakeLists.txt.fix cmake/CMakeLists.txt
+  # make:
+  cat lib/plumed/Makefile.lammps.runtime | sed "s/libplumedKernel.so/libplumedKernel.dylib/" > lib/plumed/Makefile.lammps.runtime.fix
+  mv lib/plumed/Makefile.lammps.runtime.fix lib/plumed/Makefile.lammps.runtime
+fi
+
+# blas and lapack are not required when PLUMED is linked shared or runtime:
+cat cmake/CMakeLists.txt | sed "s/ OR PKG_USER-PLUMED//" > cmake/CMakeLists.txt.fix
+mv cmake/CMakeLists.txt.fix cmake/CMakeLists.txt
+touch *
+make -C src lib-plumed args="-p ${INSTALL_DIR} -m ${PLUMED_LinkMode}"
+
+mkdir build
+cd build
+cmake -DBUILD_MPI=${BuildMPI} \
+      -D FFT=FFTW3 \
+      -D FFT_SINGLE=yes \
+      -D FFT_FFTW_THREADS=on \
+      -D FFTW3F_INCLUDE_DIR=/home/xhshi/hjx/software-self/lammps-simple/env/fftw338s/include \
+      -D FFTW3F_LIBRARY=/home/xhshi/hjx/software-self/lammps-simple/env/fftw338s/lib/libfftw3f.a \
+      -D FFTW3F_OMP_LIBRARY=/home/xhshi/hjx/software-self/lammps-simple/env/fftw338s/lib/libfftw3f_omp.a \
+      -DPKG_COLLOID=yes \
+      -DPKG_MANYBODY=yes \
+      -DPKG_KSPACE=yes \
+      -DPKG_MOLECULE=yes \
+      -DPKG_RIGID=yes \
+      -DPKG_OPENMP=yes \
+      -DPKG_USER-PLUMED=yes \
+      -DDOWNLOAD_PLUMED=no \
+      -DPLUMED_MODE=${PLUMED_LinkMode} \
+      $opt \
+      ../cmake
+#cmake -DBUILD_MPI=${BuildMPI} \
+#      -DPKG_MANYBODY=yes \
+#      -DPKG_KSPACE=yes \
+#      -DPKG_MOLECULE=yes \
+#      -DPKG_RIGID=yes \
+#      -DPKG_COLLOID=yes \
+#      -DPKG_ASPHERE=yes \
+#      -DPKG_BODY=yes \
+#      -DPKG_BOCS=yes \
+#      -DPKG_BROWNIAN=yes \
+#      -DPKG_CG_DNA=yes \
+#      -DPKG_COLVARS=yes \
+#      -DPKG_EXTRA-COMPUTE=yes \
+#      -DPKG_EXTRA-DUMP=yes \
+#      -DPKG_EXTRA-FIX=yes \
+#      -DPKG_EXTRA-MOLECULE=yes \
+#      -DPKG_EXTRA-PAIR=yes \
+#      -DPKG_MC=yes \
+#      -DPKG_MOLFILE=yes \
+#      -DPKG_OPENMP=yes \
+#      -DPKG_USER-PLUMED=yes \
+#      -DDOWNLOAD_PLUMED=no \
+#      -DPLUMED_MODE=${PLUMED_LinkMode} \
+#      $opt \
+#      ../cmake
+make VERBOSE=1 -j ${Make_CPUs}
+
+
+cp lmp ${PLUMED_INSTALL_DIR}/bin/lmp_simple
+echo "build-lammps.sh: LAMMPS has been install as lmp in the same folder as PLUMED"
+echo "build-lammps.sh: (${PLUMED_INSTALL_DIR})"
---
+++ build-plumed.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+set -e
+module purge
+module load gcc/5.4.0
+module load openmpi/3.1.4-gnu-5.4.0
+export PATH=/home/xhshi/hjx/software-self/lammps-simple/env/fftw338s/bin:$PATH
+export LD_LIBRARY_PATH=/home/xhshi/hjx/software-self/lammps-simple/env/fftw338s/lib:$LD_LIBRARY_PATH
+export BLAS=/home/xhshi/hjx/software-self/lammps-simple/env/lib/libfblas.a
+export LAPACK=/home/xhshi/hjx/software-self/lammps-simple/env/lib/liblapack.a
+# You should run this script in a clean folder 
+INSTALL_DIR=/home/xhshi/hjx/software-self/lammps-simple/old/lmp_plu
+BuildMPI=yes
+CompileWithIntel=no
+PLUMED_VERSION=2.7.4
+Make_CPUs=20
+PLUMED_CONFIG_OPT="--enable-modules=all " 
+
+# In some cases you might need to specfiy the compiler, then uncomment this line
+# CXX=mpic++
+
+# Compiler settings that seem to work with Intel compilers like the ones 
+# on the MPIP Linux workstations
+# I would recommend using the default gcc compiler but if you want to use the
+# Intel ones, see CompileWithIntel above to yes
+if [[ ${CompileWithIntel} == "yes" ]];
+then
+  CXX=mpicxx
+  CC=mpicc
+  FC=mpifc
+  CPPFLAGS="-I${MKLROOT}/include"
+  LDFLAGS="-L${MKLROOT}/lib/intel64" 
+  LIBS="-mkl"
+  CXXFLAGS="-fp-model precise -fma -align -finline-functions -no-prec-div -qoverride-limits"
+  PLUMED_CONFIG_OPT+="--disable-ld-r "
+fi
+
+if [[ ${BuildMPI} == "no" ]];
+then
+  PLUMED_CONFIG_OPT+="--disable-mpi"
+fi
+
+# Download and build PLUMED2
+if [[ -e v${PLUMED_VERSION}.zip ]]; then
+  echo "the v${PLUMED_VERSION}.zip that we want to download already exists, please delete it to be sure that the file is properly downloaded" 
+  exit 0
+fi
+if [[ -e plumed2-${PLUMED_VERSION} ]]; then
+  echo "the plumed2-${PLUMED_VERSION} from unzipping the v${PLUMED_VERSION}.zip already exists, please delete it to be sure that the build is clean"
+  exit 0 
+fi
+wget --no-cookie --no-check-certificate -e robots=off https://github.com/plumed/plumed2/archive/refs/tags/v${PLUMED_VERSION}.zip
+unzip v${PLUMED_VERSION}.zip
+rm -f  v${PLUMED_VERSION}.zip
+
+cd plumed2-${PLUMED_VERSION}
+
+if [[  -z ${INSTALL_DIR} ]];
+then 
+  INSTALL_DIR=${PWD}/install 
+fi
+
+./configure  \
+  --prefix=${INSTALL_DIR} \
+     ${PLUMED_CONFIG_OPT}
+make -j ${Make_CPUs}
+make install 
+cd ..
+
+if [[ $(uname) == Darwin ]]; then
+  SOEXT=dylib
+else 
+  SOEXT=so
+fi
+
+
+cat << EOF > source-plumed.sh
+export PLUMED_INSTALL_DIR=${INSTALL_DIR}
+export PATH=\${PLUMED_INSTALL_DIR}/bin:\${PATH}
+export INCLUDE=\${PLUMED_INSTALL_DIR}/include:\${INCLUDE}
+export CPATH=\${PLUMED_INSTALL_DIR}/include:\${CPATH}
+export LIBRARY_PATH=\${PLUMED_INSTALL_DIR}/lib:\${LIBRARY_PATH}
+export LD_LIBRARY_PATH=\${PLUMED_INSTALL_DIR}/lib:\${LD_LIBRARY_PATH}
+export PKG_CONFIG_PATH=\${PLUMED_INSTALL_DIR}/lib/pkgconfig:\${PKG_CONFIG_PATH}
+export PLUMED_KERNEL=\${PLUMED_INSTALL_DIR}/lib/libplumedKernel.${SOEXT}
+EOF
+chmod a+x source-plumed.sh
+echo ""
+echo "build-plumed.sh: PLUMED has been installed in ${INSTALL_DIR}"
+echo "build-plumed.sh:: You need to use "source source-plumed.sh" to load all the variables related to PLUMED" 
+
+
+
+
